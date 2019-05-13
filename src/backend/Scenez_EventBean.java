b68a85166ddd42da9c35c7c9a466e3fd433e7df6
Talha Zeeshan <42223121+talha-zshan@users.noreply.github.com>
2019-05-14 01:36:37 +0300

    mainfeed.jsp

---
+++ src/backend/Scenez_EventBean.java
@@ -0,0 +1,153 @@
+package backend;
+
+public class Scenez_EventBean {
+	
+	static String INVITE = "Invite";
+	static String PUBLIC = "Public";
+	
+	private int id;
+	private String name;
+	private String start_time;
+	private double duration;
+	private String privacy;
+	private int capacity;
+	private String description;
+	private String email;
+	private String cat_name;
+	private int location_id;
+	private String event_date;
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the start_time
+	 */
+	public String getStart_time() {
+		return start_time;
+	}
+	/**
+	 * @param start_time the start_time to set
+	 */
+	public void setStart_time(String start_time) {
+		this.start_time = start_time;
+	}
+	/**
+	 * @return the duration
+	 */
+	public double getDuration() {
+		return duration;
+	}
+	/**
+	 * @param duration the duration to set
+	 */
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+	/**
+	 * @return the privacy
+	 */
+	public String getPrivacy() {
+		return privacy;
+	}
+	/**
+	 * @param privacy the privacy to set
+	 */
+	public void setPrivacy(String privacy) {
+		this.privacy = privacy;
+	}
+	/**
+	 * @return the capacity
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
+	/**
+	 * @param capacity the capacity to set
+	 */
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the cat_name
+	 */
+	public String getCat_name() {
+		return cat_name;
+	}
+	/**
+	 * @param cat_name the cat_name to set
+	 */
+	public void setCat_name(String cat_name) {
+		this.cat_name = cat_name;
+	}
+	/**
+	 * @return the location_id
+	 */
+	public int getLocation_id() {
+		return location_id;
+	}
+	/**
+	 * @param location_id the location_id to set
+	 */
+	public void setLocation_id(int location_id) {
+		this.location_id = location_id;
+	}
+	/**
+	 * @return the event_date
+	 */
+	public String getEvent_date() {
+		return event_date;
+	}
+	/**
+	 * @param event_date the event_date to set
+	 */
+	public void setEvent_date(String event_date) {
+		this.event_date = event_date;
+	}
+	
+}
