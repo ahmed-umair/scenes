15c7a6d4ca2a7ffb2fbac360c680c778f204573a
Masna Ahmed <masnaahmed1998@gmail.com>
2019-05-14 02:38:15 +0300

    Create Friend_Pair.java

---
+++ src/backend/Friend_Pair.java
@@ -0,0 +1,32 @@
+package backend;
+
+public class Friend_Pair {
+	
+	private String requester;
+	private String requested;
+	/**
+	 * @return the requester
+	 */
+	public String getRequester() {
+		return requester;
+	}
+	/**
+	 * @param requester the requester to set
+	 */
+	public void setRequester(String requester) {
+		this.requester = requester;
+	}
+	/**
+	 * @return the requested
+	 */
+	public String getRequested() {
+		return requested;
+	}
+	/**
+	 * @param requested the requested to set
+	 */
+	public void setRequested(String requested) {
+		this.requested = requested;
+	}
+
+}
